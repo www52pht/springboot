89bd34e9e55277c5d0377fc8ae60c31f9faba073
wangweiwei <47584410+www52pht@users.noreply.github.com>
2020-05-16 14:30:56 +0800

    用@RestController替换

--- src/main/java/com/example/springboot/Controller/helloController.java
+++ src/main/java/com/example/springboot/Controller/helloController.java
@@ -5,20 +5,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+
 /**
+ * @RestController可以取代下面两个注解（可以进@RestController里面就明白了）
  * @ResponseBody
  * @Controller
- * 上面这两个注解可以用@RestController代替(因为restcontroller这个注解里面也是
- * @Target({ElementType.TYPE})
- * @Retention(RetentionPolicy.RUNTIME)
- * @Documented
- * @Controller
- * @ResponseBody几个注解组成)
  */
 @RestController
 public class helloController {
     @RequestMapping("/hello")
     public String hello(){
-        return "你开心就行！";
+        return "你开心就行！23333";
     }
 }
